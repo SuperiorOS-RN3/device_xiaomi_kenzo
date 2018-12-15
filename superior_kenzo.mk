81f6a488d394e13bf5305e85b8208d3d35137590
Angga R Saputra <45820088+SentinelP@users.noreply.github.com>
2018-12-15 15:04:46 +0700

    Update and rename aokp_kenzo.mk to superior_kenzo.mk

--- superior_kenzo.mk
+++ superior_kenzo.mk
@@ -16,15 +16,15 @@
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 
-# Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/configs/common.mk)
 
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := aokp_kenzo
+PRODUCT_NAME := superior_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
